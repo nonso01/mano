3283dab311f8cc9d409b8f15f688625ff42ebfc7
nonso01 <chukwunonsomartin01@gmail.com>
2024-01-11 22:27:54 +0100

    remove unwanted

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,1 @@
 #![allow(dead_code)]
-
-pub const MANO_VERSION: &str = "0.1.0";
-
-#[derive(Debug)]
-pub struct NothingForNow {
-  pub nothing: String,
-  pub  _for: String,
-  pub  now: String,
-}
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,5 @@
 #![allow(dead_code)]
 
-use mano::NothingForNow;
-
 fn main() {
-    let a = NothingForNow {
-        nothing: String::from("hello mano"),
-        _for: String::from("am gonna"),
-        now: String::from("build you"),
-    };
-
-    println!("{:#?}", a);
+    println!("{:#?}", String::from("Style your web app with less noise"));
 }
