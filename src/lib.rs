45234abed2f51dcc7f826a23402e85422ae813d2
nonso01 <chukwunonsomartin01@gmail.com>
2024-02-03 16:34:08 +0100

    working on the cli options

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,29 @@
 #![allow(dead_code)]
-// use std::*;
-
 const VERSION: &str = "mano v0.1.0";
 
 /// The first task of this project will be the creation of a robust command line interface.
  pub mod mano_cli {
-     pub fn cli() {
+     // use std::{self, collections::{HashMap}};
 
+     pub struct CommandOptions<S, E>  {
+         pub help: S,
+         pub version: S,
+         pub init: E,
+         pub watch: E,
+         pub minify: E,
+         pub clean: E,
+         pub update: E,
      }
+
+     pub struct CommandOptionStyle {
+         // using anstyle
+     }
+
+     pub trait DefaultOptions {
+         fn display_help(&self) -> String;
+     }
+
+     pub fn cli() {}
  }
 
 /// The second of this project will be the implementation of useful filesystem functions
