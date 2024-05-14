2409d9638258ec658a6add03c420d42600d5c28e
nonso01 <chukwunonsomartin01@gmail.com>
2024-05-14 12:46:18 +0100

    more command line features

--- src/lib.rs
+++ src/lib.rs
@@ -1,40 +1,48 @@
 #![allow(dead_code)]
 const VERSION: &str = "v0.1.0";
 
-// The first task of this project,
+// (1) The first task of this project,
 // will be the creation of a robust command line interface.
 
 #[allow(unused_imports)]
 use ::clap::{
     arg,
     builder::{styling::AnsiColor, Styles},
-    Arg, Command,
+    value_parser, Arg, ArgAction, ArgMatches, Command,
 };
 
-pub fn cli() {
+pub fn cli() -> ArgMatches {
+    let about_crate = String::from("Mano is a powerful and blazing-fast utility-first css generation library\nwritten in Rust, it offers significant enhancement in performance, flexibility and feature set.");
+
     let display_styles = Styles::styled()
         .header(AnsiColor::Yellow.on_default())
         .usage(AnsiColor::Blue.on_default())
         .literal(AnsiColor::Cyan.on_default());
 
-    let _matches = Command::new("mano.rs")
+    let matches = Command::new("mano")
         .author("Nonso Martin")
         .version(env!("CARGO_PKG_VERSION"))
-        .about(
-            "\tmano.rs is a standalone software crafted for generating css styles on the go!,\
-                    \n\tbuilt with rust for more efficiency and productivity.",
-        )
+        .about(&about_crate)
         .args([
-            arg!(-w --watch "Watches the target file(s) for changes"),
+            arg!(-w --watch "Watches the target file(s) for changes")
+                .value_parser(value_parser!(bool)),
             arg!(-m --minify "Minifies the output file(s)"),
             arg!(-i --init "Creates a json file with specific settings"),
             arg!(-u --unused "Clean up unsued variables and style declarations"),
-            arg!(-s --search "Search for base or default styles"),
-            arg!(-t --tab "Opens a new browser window, displaying all curreent activities"),
+            Arg::new("search")
+                .short('s')
+                .long("search")
+                .action(ArgAction::Set)
+                .value_delimiter(',')
+                .num_args(1..5)
+                .help("Searches the program for utility classes, fonts, colors, and more"),
+            arg!(-t --tab "Opens a new browser window, displaying all current activities"),
         ])
         .styles(display_styles)
         .arg_required_else_help(true) // exp
         .get_matches();
+
+    matches
 }
 
 // The second task of this project will be the implementation of useful filesystem functions
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,17 @@
 #![allow(dead_code)]
 use mano::cli;
 
+const CONFIG_FILLE: &str = "mano.json";
+
 fn main() {
-    cli();
+    // (1) work on the command line
+    let _cmd = cli();
+
+    // we initialize our project
+    let __init__ = match _cmd.get_one::<bool>("init") {
+        Some(initialize) => initialize,
+        _ => todo!(),
+    };
+
+    println!("{__init__}");
 }
