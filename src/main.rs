6c3f155268e325fb7a1111a1e46148037763c62b
nonso01 <chukwunonsomartin01@gmail.com>
2024-05-17 13:02:30 +0100

    fix

--- src/main.rs
+++ src/main.rs
@@ -6,20 +6,20 @@ const CONFIG_FILE: &str = "mano.config.json";
 
 fn main() {
     // (1) work on the command line
-    let _cmd = cli();
+    let cmd = cli();
 
-    let _cmd_options = CommandOptions {
-        watch: _cmd.get_one::<bool>("watch").unwrap(),
-        minify: _cmd.get_one::<bool>("minify").unwrap(),
-        init: _cmd.get_one::<bool>("init").unwrap(),
-        unused: _cmd.get_one::<bool>("unused").unwrap(),
-        browser: _cmd.get_one::<bool>("browser").unwrap(),
-        search: match _cmd.get_one::<String>("search") {
+    let cmd_options = CommandOptions {
+        watch: cmd.get_one::<bool>("watch").unwrap(),
+        minify: cmd.get_one::<bool>("minify").unwrap(),
+        init: cmd.get_one::<bool>("init").unwrap(),
+        unused: cmd.get_one::<bool>("unused").unwrap(),
+        browser: cmd.get_one::<bool>("browser").unwrap(),
+        search: match cmd.get_one::<String>("search") {
             /* we need a Vec */
             Some(query) => query,
             None => "NO QUERY",
         },
     };
 
-    println!("{:#?}", _cmd_options);
+    println!("{:#?}", cmd_options);
 }
